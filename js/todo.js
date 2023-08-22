27f9578d99feef59189e5f78fa7af080e0794ee1
hyejigpwl <98373383+hyejigpwl@users.noreply.github.com>
2023-08-22 13:58:13 +0900

    todo 로딩,삭제

--- js/todo.js
+++ js/todo.js
@@ -2,22 +2,27 @@ const todoFrm = document.querySelector("#todo_form");
 const todoIpt = document.querySelector("#todo_form input");
 const todoList = document.querySelector("#todo_list");
 
-const toDos = [];
+const TODOS_KEY = "todos";
+
+let toDos = [];
 
 function saveTodo(){
-    localStorage.setItem("todos",JSON.stringify(toDos));
+    localStorage.setItem(TODOS_KEY,JSON.stringify(toDos));
 }
 
 function deleteTodo(event){
     const li = event.target.parentElement;
     li.remove();
+    toDos = toDos.filter((toDo) => toDo.id !== parseInt(li.id));
+    saveTodo();
 }
 
 
 function paintTodo(newTodo){
     const li = document.createElement("li");
+    li.id = newTodo.id;
     const span = document.createElement("span");
-    span.innerText = newTodo;
+    span.innerText = newTodo.text;
     const button = document.createElement("button");
     button.innerText = "✖";
     button.addEventListener("click",deleteTodo);
@@ -30,9 +35,22 @@ function todoSubmit(event){
     event.preventDefault();
     const newTodo = todoIpt.value;
     todoIpt.value = "";
-    toDos.push(newTodo);
-    paintTodo(newTodo);
+    const newTodoObj = {
+        text:newTodo,
+        id:Date.now(),
+    };
+    toDos.push(newTodoObj);
+    paintTodo(newTodoObj);
     saveTodo();
 }
 
 todoFrm.addEventListener("submit",todoSubmit);
+
+
+const savedToDos = localStorage.getItem(TODOS_KEY);
+
+if(savedToDos){
+    const parsedToDos = JSON.parse(savedToDos); 
+    toDos = parsedToDos;
+    parsedToDos.forEach(paintTodo);
+}
